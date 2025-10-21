9fb32a7d97dc00216ac87ad29be116a7008d7259
willywack32-maker <willywack32@gmail.com>
2025-10-21 19:39:00 +1300

    same update
    update

--- Program.cs
+++ Program.cs
@@ -2,52 +2,33 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Convert PostgreSQL URL to standard connection string
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-if (connectionString.StartsWith("postgresql://"))
-{
-    // Convert from: postgresql://user:password@host:port/database
-    // To: Host=host;Port=port;Database=database;Username=user;Password=password
-    var uri = new Uri(connectionString);
-    var db = uri.AbsolutePath.Trim('/');
-    var user = uri.UserInfo.Split(':')[0];
-    var passwd = uri.UserInfo.Split(':')[1];
-    
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={db};Username={user};Password={passwd};SSL Mode=Require;Trust Server Certificate=true";
-}
-
-// Add services to the container
+// Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configure PostgreSQL
+// Database (using in-memory for now)
 builder.Services.AddDbContext<PickeAPIContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseInMemoryDatabase("PickerDB"));
 
 var app = builder.Build();
 
-// Add this - automatically run migrations on startup
-//using (var scope = app.Services.CreateScope())
-//{
-  //  var db = scope.ServiceProvider.GetRequiredService<PickeAPIContext>();
-    //db.Database.Migrate();
-//}
+// Add test endpoints FIRST
+app.MapGet("/", () => "API Root - Working!");
+app.MapGet("/test", () => "Test endpoint - Working!");
+app.MapGet("/api/test", () => "API Test - Working!");
 
-// Configure the HTTP request pipeline
+// Then map controllers
+app.MapControllers();
+
+// Development features
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Add these test endpoints:
-app.MapGet("/", () => "Root works!");
-app.MapGet("/test", () => "Test works!"); 
-app.MapGet("/api/test", () => "API test works!");
 app.UseHttpsRedirection();
 app.UseAuthorization();
-app.MapControllers();
 
 app.Run();
